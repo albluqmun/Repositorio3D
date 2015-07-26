aae5896bfdb0988d3a645395c8b30beac61fa084
albluqmun <albluqmun@gmail.com>
2015-07-26 20:11:17 +0200

    Definiendo la estructura visual de la web

--- Repositorio3D/Repositorio3D/settings.py
+++ Repositorio3D/Repositorio3D/settings.py
@@ -114,3 +114,5 @@ EMAIL_PORT = 1025
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "Repositorio3D", "static"),)
