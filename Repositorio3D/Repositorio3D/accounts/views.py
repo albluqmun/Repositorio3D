7102617863fffa2bf99df61139e96fdfea34ae53
albluqmun <albluqmun@gmail.com>
2016-02-27 13:51:12 +0100

    Login parte superior

--- Repositorio3D/Repositorio3D/accounts/views.py
+++ Repositorio3D/Repositorio3D/accounts/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import TemplateView
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from registration.backends.default.views import RegistrationView
 from registration.forms import RegistrationFormUniqueEmail
 
 
-class PerfilUsuario(TemplateView):
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+
+
+class PerfilUsuario(LoginRequiredMixin, TemplateView):
     template_name = "accounts/perfil.html"
 
 
